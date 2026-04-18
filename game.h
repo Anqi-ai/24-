2fc6f775b03d6595760e89a2a5ac22679caa0381
Anqi-ai <B4236691@ulive.pccu.edu.tw>
2026-04-18 20:24:50 +0800

    Create game.h

---
+++ game.h
@@ -0,0 +1,10 @@
+#ifndef GAME_H
+#define GAME_H
+#include<vector>
+#include<string>
+using namespace std;
+class Game24 {
+public:
+    bool hasSolution(vector<double> nums, vector<string> expr);
+};
+#endif
